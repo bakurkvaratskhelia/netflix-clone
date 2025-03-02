94a281eabb51a66e19232d5f6b5d02187e45e7af
bakurkvaratskhelia <bakurikv@yahoo.com>
2025-03-02 23:28:09 +0300

    initial commit

---
+++ frontend/src/utils/dateFunction.js
@@ -0,0 +1,8 @@
+export function formatReleaseDate(date) {
+    return new Date(date).toLocaleDateString("en-Us", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+
+    });
+}
