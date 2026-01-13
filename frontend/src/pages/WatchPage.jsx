e52a562b5e4ca0a950c780f344cc469af87d4650
bakurkvaratskhelia <bakurikv@yahoo.com>
2026-01-13 19:10:00 +0300

    Debug: Add iframe for troubleshooting YouTube embed issue in WatchPage

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -164,13 +164,25 @@ const WatchPage = () => {
 
         <div className='aspect-video mb-8 p-2 sm:px-10 md:px-32'>
           {currentTrailer && currentTrailer.key ? (
-            <ReactPlayer
-              controls
-              width='100%'
-              height='70vh'
-              className='mx-auto overflow-hidden rounded-lg'
-              url={`https://www.youtube.com/watch?v=${currentTrailer.key}`}
-            />
+            <>
+              <ReactPlayer
+                controls
+                width='100%'
+                height='70vh'
+                className='mx-auto overflow-hidden rounded-lg'
+                url={`https://www.youtube.com/watch?v=${currentTrailer.key}`}
+              />
+              {/* Add iframe for debugging */}
+              <iframe
+                width='560'
+                height='315'
+                src='https://www.youtube.com/embed/aTAacTUKK00'
+                frameBorder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
+                title='Test Trailer'
+              />
+            </>
           ) : (
             <h2 className='text-xl text-center mt-5'>
               No trailers available for{' '}
